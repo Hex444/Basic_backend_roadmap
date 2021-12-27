48fe90547ec8a5f211d92a51bfa6fea68cbb4307
realhex444 <ayaansanjeev@gmail.com>
2021-12-27 14:59:23 +0530

    too much stuff to say

---
+++ custommodule/mod.js
@@ -0,0 +1,15 @@
+// console.log('this is module');
+
+function average(arr){
+    let sum = 0;
+    arr.forEach(element => {
+        sum += element;
+    });
+    return sum/arr.length
+}
+
+module.exports = {
+    avg: average,
+    name: 'Hex444',
+    repo: 'GitHub'
+}
---
+++ usingnpm/usingnpm.js
@@ -0,0 +1,8 @@
+console.log('this is usingnpm.js 1.0.1')
+
+// version 1.0.0
+// major, minor, patch
+
+// --save-dev for making a devdependency (a moduleonly needed for production and not deployment)
+// --global when you need to install something to use globally 
+// can use |npm i| instead of |npm install|
